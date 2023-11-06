fab8746288921cd72fd674905e94566ad9e1f3b8
Alexei Torgashov <alexei.torgashov@yahoo.de>
2023-11-06 22:04:39 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import * as Avatar from "$lib/components/ui/avatar";
+</script>
+
+<div class="grid w-full justify-center gap-12 mt-12">
+    <Avatar.Root class='w-32 h-32'>
+      <Avatar.Image src="https://picsum.photos/200" alt="random-pic" crossorigin="anonymous" />
+      <Avatar.Fallback>Not loaded</Avatar.Fallback>
+    </Avatar.Root>
+
+    <img 
+        class="w-32 h-32 rounded-full"
+        src="https://picsum.photos/200"
+        alt="random-pic"
+        crossorigin="anonymous"
+    />
+
+    <Avatar.Root class='w-32 h-32'>
+      <Avatar.Image src="https://github.com/huntabyte.png" alt="@huntabyte" crossorigin="anonymous" />
+      <Avatar.Fallback>Not loaded</Avatar.Fallback>
+    </Avatar.Root>
+
+    <img
+        class="w-32 h-32 rounded-full"
+        src="https://github.com/huntabyte.png"
+        alt="@huntabyte"
+        crossorigin="anonymous"
+    />
+</div>
